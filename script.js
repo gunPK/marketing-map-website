a7380aba9b35182bbf7a9021910c9197250649fb
gunPK <147191381+gunPK@users.noreply.github.com>
2024-03-07 10:12:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,34 +7,27 @@ const map = new mapboxgl.Map({
     zoom: 3 // Default zoom level
 });
 
-map.on('load', function () {
-  // Existing map initialization code
-
-  // Initialize the Geocoder
-  var geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl,
-    marker: false // Set to true if you want a marker to appear at the search location
-  });
-
-  // Add the geocoder to your map
-  document.getElementById('map').appendChild(geocoder.onAdd(map));
-});
-
-
 map.on('load', () => {
+    // Initialize the Geocoder
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false // Set to true if you want a marker to appear at the search location
+    });
+
+    // Add the geocoder to your map
+    document.getElementById('map').appendChild(geocoder.onAdd(map));
+
+    // Move reset button to the left of the search bar (Geocoder)
+    document.querySelector('.mapboxgl-ctrl-geocoder').insertAdjacentElement('beforebegin', document.getElementById('reset-button'));
+
     // Event listener for clicking on a feature within the specified layer
     map.on('click', 'merged-data-points-7n4pjn', (e) => {
-        // Ensure that the clicked feature's coordinates are in a format that can be used
         const coordinates = e.features[0].geometry.coordinates.slice();
-        // Access the properties of the clicked feature
         const properties = e.features[0].properties;
         
-        // Example description - customize this to include the information you want to display
-        // This assumes your feature has a property named 'NAME'
         const description = `<h4>${properties.NAME}</h4><p>Message Count: ${properties.message_count}<br>Contractor Count: ${properties.contractor_count}</p>`;
         
-        // Create and show a popup with the feature's information
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(description)
@@ -50,13 +43,13 @@ map.on('load', () => {
     map.on('mouseleave', 'merged-data-points-7n4pjn', () => {
         map.getCanvas().style.cursor = '';
     });
-});
 
-document.getElementById('reset-button').addEventListener('click', function() {
-    map.flyTo({
-        center: [-95.7129, 37.0902], // Original center
-        zoom: 3, // Original zoom level
-        essential: true // This ensures the animation is performed
+    // Reset button functionality to move the map back to the initial position
+    document.getElementById('reset-button').addEventListener('click', function() {
+        map.flyTo({
+            center: [-95.7129, 37.0902], // Original center
+            zoom: 3, // Original zoom level
+            essential: true // This ensures the animation is performed
+        });
     });
 });
-
